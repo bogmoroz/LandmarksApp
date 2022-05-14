bd37ad69d42d1500e55a498a09e1c3341c428c1f
Bogdan Moroz <bogdan.moroz@observis.fi>
2022-05-14 10:18:08 +0300

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Bogdan Moroz on 14.5.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
